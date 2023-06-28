484eb79529efef3b25a53bc154b2fed2b90da982
Deathlyjoy <colombelle.quentin@icloud.com>
2023-06-28 10:27:15 +0200

    Etape 1.1 : ajout du fetch Works
    Appel de l'API avec fetch afin de récupérer dynamiquement les
    projets de l’architecte.

--- FrontEnd/assets/js/app.js
+++ FrontEnd/assets/js/app.js
@@ -1,1 +1,14 @@
 // API
+async function fetchWorks() {
+    const response = await fetch("http://localhost:5678/api/works");
+    if (response.ok === true) {
+        console.log("Connexion API : Successful");
+        return response.json();
+      } else {
+        console.log("Error Connexion API : " + response.error);
+      }
+    // const data = await response.json();
+    // console.log(data);
+    // return data;
+}
+console.log(fetchWorks());
