16ba227f7e81f78d5457250f28797d7bfeb4dbab
Deathlyjoy <colombelle.quentin@icloud.com>
2023-06-28 09:27:27 +0200

    Initial HTML/CSS Login
    Structure générique de la page login

--- FrontEnd/assets/js/app.js
+++ FrontEnd/assets/js/app.js
@@ -1,7 +1,1 @@
 // API
-async function works() {
-    const response = await fetch("http://localhost:5678/api/works");
-    const dataWorks = await response.json();
-    console.log(dataWorks);
-    return dataWorks;
-}
