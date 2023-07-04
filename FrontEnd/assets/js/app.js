e8598523ac3c48b2669740098007e639323ef0e6
Deathlyjoy <colombelle.quentin@icloud.com>
2023-07-04 16:40:07 +0200

    Add Filter inde.html
    Ajout des filtres des projets sur la page principale

--- FrontEnd/assets/js/app.js
+++ FrontEnd/assets/js/app.js
@@ -24,6 +24,10 @@ console.log(dataCategories());
 // VARIABLES
 const gallery = document.querySelector("#gallery")
 
+const buttonGeneric = document.querySelector("#btn-generic");
+const allProject = document.querySelectorAll("#gallery > div");
+
+const btn = document.getElementsByClassName("button");
 
 // PROJECT
 async function project() {
@@ -40,3 +44,59 @@ async function project() {
   });
 }
 project();
+
+// BUTTON "TOUS"
+const buttonT = document.createElement("button");
+buttonT.setAttribute("categoryId", "0");
+buttonT.innerText = "Tous";
+buttonT.setAttribute("class", "button");
+buttonT.addEventListener("click", () => {
+  allProject.forEach((div) => (div.style.display = "block"));
+});
+buttonGeneric.appendChild(buttonT);
+
+// BUTTONS FILTER
+async function button() {
+  const dataButton = await dataCategories();
+  console.log(dataButton);
+  dataButton.forEach((btn) => {
+    const btnSite = document.createElement("button");
+    btnSite.innerText = `${btn.name}`;
+    btnSite.setAttribute("class", "button");
+    btnSite.setAttribute("categoryId", `${btn.id}`);
+    buttonGeneric.appendChild(btnSite);
+  });
+}
+button();
+
+// FILTER
+async function filtreTravaux() {
+  const dataFiltreTravaux = await dataWorks();
+  const buttonBis = await dataCategories();
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", () => {
+      console.log(btn[i]);
+      let ctgId = btn[i].getAttribute("categoryId");
+      console.log(ctgId);
+      if (ctgId == 0) {
+        gallery.innerHTML = "";
+        project();
+      } else {
+        gallery.innerHTML = "";
+        dataFiltreTravaux.forEach((galleryImg) => {
+          if (galleryImg.categoryId == ctgId) {
+            const imgProjet = document.createElement("div");
+            const imgSophie = document.createElement("img");
+            const titleSophie = document.createElement("h3");
+            imgSophie.src = `${galleryImg.imageUrl}`;
+            titleSophie.innerText = `${galleryImg.title}`;
+            imgProjet.appendChild(imgSophie);
+            imgProjet.appendChild(titleSophie);
+            gallery.appendChild(imgProjet);
+          }
+        });
+      }
+    });
+  }
+}
+filtreTravaux();
